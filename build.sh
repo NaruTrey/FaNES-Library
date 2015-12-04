249c3611a276c16053bd846abc417ac678828477
Andrey K <NaruTrey@users.noreply.github.com>
2015-12-04 22:21:28 +0400

    Initial commit

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 LIBRARY_NAME="FaNES"
 
-rm build/* >> /dev/null
+rm build/ -R >> /dev/null 2>&1
+mkdir build/
+
 for asmFile in `find . -type f -name "*.s"`
 do
 	ca65 $asmFile -o build/`basename -s .s $asmFile`.o
 done
 
-rm out/"$LIBRARY_NAME".lib
+rm out/"$LIBRARY_NAME".lib >> /dev/null 2>&1
 ar65 a out/"$LIBRARY_NAME".lib `find ./build -type f -name "*.o"`
