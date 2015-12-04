3e74d9932520174bef6a11819b64a95637fb65aa
Andrey K <NaruTrey@users.noreply.github.com>
2015-12-04 22:37:20 +0400

    Initial commit

---
+++ FaNES.h
@@ -0,0 +1,28 @@
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
+#define TRUE 	1
+#define FALSE 	0
+#define NULL	0
+
+#define PALETTE_BACKGROUND_1    0
+#define PALETTE_BACKGROUND_2    0x04
+#define PALETTE_BACKGROUND_3    0x08
+#define PALETTE_BACKGROUND_4    0x0B
+#define PALETTE_SPRITE_1        0x10
+#define PALETTE_SPRITE_2        0x14
+#define PALETTE_SPRITE_3        0x18
+#define PALETTE_SPRITE_4        0x1B
+
+void fastcall setPaletteColor(uchar index, uchar color);
+
+void fastcall onGraphics(void);
+
+void fastcall waitFrame(void);
+
+#define NAMETABLE_A_POSITION(x, y) 	(0x2000 | ((y << 5) | x))
+#define NAMETABLE_B_POSITION(x, y) 	(0x2400 | ((y << 5) | x))
+#define NAMETABLE_C_POSITION(x, y) 	(0x2800 | ((y << 5) | x))
+#define NAMETABLE_D_POSITION(x, y) 	(0x2C00 | ((y << 5) | x))
+
+void fastcall printNametableString(uint coordinates, uchar *string);
