2b9a9fbc708363b455370f68c79eebcf4223abcf
NaruTrey <me@ntrey.org>
2015-12-12 10:06:18 +0400

    Format FaNES.h

--- FaNES.h
+++ FaNES.h
@@ -8,23 +8,14 @@ typedef unsigned int uint;
 #define MSB(x)  (x >> 8)
 #define LSB(x)  (x & 0xFF)
 
-
-void fastcall setSpritesBank(uchar bank);
-
-void fastcall copy(void *destination, void *source, uint length);
-
 #define UPDATE_NAMETABLE_HORIZONTAL		0x40
 #define UPDATE_NAMETABLE_VERTICAL		0x80
 #define UPDATE_NAMETABLE_END		    0xFF
-
-void fastcall updateNametable(uchar *buffer);
 
 #define NAMETABLE_A		0x2000
 #define NAMETABLE_B		0x2400
 #define NAMETABLE_C		0x2800
 #define NAMETABLE_D		0x2c00
-
-void fastcall unpackNametableRLE(const uchar *data, uint nametable);
 
 #define PAD_PORT_PLAYER_1   0
 #define PAD_PORT_PLAYER_2	1
@@ -37,9 +28,6 @@ void fastcall unpackNametableRLE(const uchar *data, uint nametable);
 #define PAD_BUTTON_LEFT		0x40
 #define PAD_BUTTON_RIGHT	0x80
 
-uchar fastcall padPool(uchar port);
-uchar fastcall padPush(uchar port);
-
 #define PALETTE_BACKGROUND_1    0
 #define PALETTE_BACKGROUND_2    0x04
 #define PALETTE_BACKGROUND_3    0x08
@@ -48,6 +36,27 @@ uchar fastcall padPush(uchar port);
 #define PALETTE_SPRITE_2        0x14
 #define PALETTE_SPRITE_3        0x18
 #define PALETTE_SPRITE_4        0x1B
+
+#define METASPRITE_END  128
+
+#define NAMETABLE_A_POSITION(x, y) 	(0x2000 | ((y << 5) | x))
+#define NAMETABLE_B_POSITION(x, y) 	(0x2400 | ((y << 5) | x))
+#define NAMETABLE_C_POSITION(x, y) 	(0x2800 | ((y << 5) | x))
+#define NAMETABLE_D_POSITION(x, y) 	(0x2C00 | ((y << 5) | x))
+
+extern uchar _frameCounter;
+#pragma zpsym("_frameCounter");
+
+void fastcall setSpritesBank(uchar bank);
+
+void fastcall copy(void *destination, void *source, uint length);
+
+void fastcall printNametableString(uint coordinates, uchar *string);
+void fastcall updateNametable(uchar *buffer);
+void fastcall unpackNametableRLE(const uchar *data, uint nametable);
+
+uchar fastcall padPool(uchar port);
+uchar fastcall padPush(uchar port);
 
 void fastcall setPalette(const uchar *data);
 void fastcall setPaletteBackground(const uchar *data);
@@ -59,22 +68,10 @@ uchar fastcall getRandomUchar(void);
 uchar fastcall newSprite(uchar x, uchar y, uchar charaster, uchar attributes, uchar spriteID);
 uchar fastcall setSpritePosition(uchar x, uchar y, uchar spriteID);
 
-#define METASPRITE_END  128
-
 uchar fastcall newMetasprite(uchar x, uchar y, uchar spriteID, const uchar *data);
 uchar fastcall setMetaspritePosition(uchar x, uchar y, uchar spriteID, const uchar *data);
 
 void fastcall offGraphics(void);
 void fastcall onGraphics(void);
 
-extern uchar _frameCounter;
-#pragma zpsym("_frameCounter");
-
 void fastcall waitFrame(void);
-
-#define NAMETABLE_A_POSITION(x, y) 	(0x2000 | ((y << 5) | x))
-#define NAMETABLE_B_POSITION(x, y) 	(0x2400 | ((y << 5) | x))
-#define NAMETABLE_C_POSITION(x, y) 	(0x2800 | ((y << 5) | x))
-#define NAMETABLE_D_POSITION(x, y) 	(0x2C00 | ((y << 5) | x))
-
-void fastcall printNametableString(uint coordinates, uchar *string);
