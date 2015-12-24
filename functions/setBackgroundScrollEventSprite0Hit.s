20dce43e1d31e37cda7781307e2a242b17ba9cde
NaruTrey <me@ntrey.org>
2015-12-24 17:15:44 +0400

    Format

--- functions/setBackgroundScrollEventSprite0Hit.s
+++ functions/setBackgroundScrollEventSprite0Hit.s
@@ -1,10 +1,10 @@
 .zeropage
 .import TEMP_VAR, PPU_CTRL_VAR
 
-SX = TEMP_VAR
-SY = TEMP_VAR + 1
-ADDR1 = SY + 1
-ADDR2 = ADDR1 + 1
+SX_VAR = TEMP_VAR
+SY_VAR = TEMP_VAR + 1
+ADDR1_VAR = SY_VAR + 1
+ADDR2_VAR = ADDR1_VAR + 1
 
 .code
 .import popa, PPU_STATUS, PPU_SCROLL, PPU_ADDR
@@ -12,26 +12,26 @@ ADDR2 = ADDR1 + 1
 .export _setBackgroundScrollEventSprite0Hit
 ;void fastcall setBackgroundScrollEventSprite0Hit(uchar x, uchar y);
 _setBackgroundScrollEventSprite0Hit:
-    sta <SY
+    sta <SY_VAR
     jsr popa
-    sta <SX
+    sta <SX_VAR
     lda <PPU_CTRL_VAR
     and #%00000011
-    sta <ADDR1
-    lda <SY
+    sta <ADDR1_VAR
+    lda <SY_VAR
     asl
     asl
     and #%11100000
-    sta <ADDR2
-    lda <SX
+    sta <ADDR2_VAR
+    lda <SX_VAR
     lsr
     lsr
     lsr
-    ora <ADDR2
-    sta <ADDR2
-    ldx <SX
-    ldy <SY
-    lda <ADDR1
+    ora <ADDR2_VAR
+    sta <ADDR2_VAR
+    ldx <SX_VAR
+    ldy <SY_VAR
+    lda <ADDR1_VAR
 @1:
     bit PPU_STATUS
     bvs @1
@@ -41,6 +41,6 @@ _setBackgroundScrollEventSprite0Hit:
     sta PPU_ADDR
     sty PPU_SCROLL
     stx PPU_SCROLL
-    lda <ADDR2
+    lda <ADDR2_VAR
     sta PPU_ADDR
     rts
